dbe3fcda65b210ffe6a26843c0f1785a6bfe840b
PdskDev <prestadesk@gmail.com>
2022-09-01 20:42:51 +0200

    first component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <current-time class="col-4" />
+    <task-input class="col-6" @add-task="addNewTask" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import CurrentTime from './components/CurrentTime.vue';
+import TaskInput from './components/TaskInput.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    CurrentTime,
+    TaskInput,
+  },
+  methods: {
+    addNewTask(task) {
+      alert(`New task added: ${task}`);
+    },
   },
 };
 </script>
